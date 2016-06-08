2fd48e756848c3c34ecf1d8a6d0638042731039c
Ilua96 <sspsps00@gmail.com>
2016-06-08 17:31:49 +1000

    Revert "Add tests and complete the lib"
    This reverts commit 7adc36838a06a17b819b375eddb64fe970ed867f.

--- Complex/Complex.h
+++ Complex/Complex.h
@@ -1,61 +1,28 @@
 #pragma once
-#define _USE_MATH_DEFINES
-#include <math.h>
 
 namespace ComplexLib
 {
 	class complex
 	{
 	public:
-		complex() : x(0.0), y(0.0){};
-		complex(double x, double y) : x(x), y(y){};
+		complex(float x, float y) : x(x), y(y){};
+		complex(){};
 		~complex(){};
 		complex operator+(const complex& src);
-		complex operator+(const double n);
+		complex operator+(const float n);
 		complex operator-(const complex& src);
-		complex operator-(const double n);
+		complex operator-(const float n);
 		complex operator*(const complex& src);
-		complex operator*(const double n);
+		complex operator*(const float n);
 		complex operator/(const complex& src);
-		complex operator/(const double n);
+		complex operator/(const float n);
 		complex operator=(const complex& src);
-		complex operator=(const double n);
+		complex operator=(const float n);
 		bool operator==(const complex& src);
-		bool operator==(const double n);
-		virtual double abs();
-		complex pow(const int n);
-		double x;
-		double y;
-	};
-
-	class ComplexPolar : public complex
-	{
-	public:
-		ComplexPolar() : r(0.0), fi(0.0) {};
-		ComplexPolar(double r, double fi) : r(r), fi(fi) {};
-		ComplexPolar(complex src){
-			if (src == 0)
-				return;
-			r = src.abs();
-			fi = atan2(src.y, src.x);
-		};
-		~ComplexPolar(){};
-		static complex ToComplex(const ComplexPolar& src);
-		ComplexPolar operator+(const ComplexPolar& src);
-		ComplexPolar operator+ (const double n);
-		ComplexPolar operator-(const ComplexPolar& src);
-		ComplexPolar operator- (const double n);
-		ComplexPolar operator*(const ComplexPolar& src);
-		ComplexPolar operator*(const double n);
-		ComplexPolar operator/(const ComplexPolar& src);
-		ComplexPolar operator/(const double n);
-		ComplexPolar operator=(const ComplexPolar& src);
-		ComplexPolar operator=(const double n);
-		bool operator==(const ComplexPolar& src);
-		bool operator==(const double n);
-		double abs();
-		complex pow(const int n);
-		double r;
-		double fi;
+		bool operator==(const float n);
+		virtual float abs();
+	private:
+		float x;
+		float y;
 	};
 }
--- Complex/FunctionComplex.cpp
+++ Complex/FunctionComplex.cpp
@@ -10,7 +10,7 @@ namespace ComplexLib
 		return complex(x + src.x, y + src.y);
 	}
 
-	complex complex::operator+(const double n){
+	complex complex::operator+(const float n){
 		return complex(x + n, y);
 	}
 
@@ -18,36 +18,32 @@ namespace ComplexLib
 		return complex(x - src.x, y - src.y);
 	}
 
-	complex complex::operator-(const double n){
+	complex complex::operator-(const float n){
 		return complex(x - n, y);
 	}
 
 	complex complex::operator*(const complex& src){
-		return complex(x * src.x - y * src.y, y * src.x + x * src.y);
+		return complex(x * src.x - y * src.y, x * src.y + y * src.x);
 	}
 
-	complex complex::operator*(const double n){
+	complex complex::operator*(const float n){
 		return complex(x * n, y * n);
 	}
 
 	complex complex::operator/(const complex& src){
-		return complex((x * src.x + y * src.y) / (src.x * src.x + src.y * src.y), (y * src.x - x * src.y) / (src.x * src.x + src.y * src.y));
+		return complex((x * src.x + y * src.y) / (src.x * src.x + src.y * src.y), (x * src.x - y * src.y) / (src.x * src.x + src.y * src.y));
 	}
 
-	complex complex::operator/(const double n){
+	complex complex::operator/(const float n){
 		return complex(x / n, y / n);
 	}
 
 	complex complex::operator=(const complex& src){
-		x = src.x;
-		y = src.y;
-		return *this;
+		return complex(src.x, src.y);
 	}
 
-	complex complex::operator=(const double n){
-		x = n;
-		y = 0.0;
-		return *this;
+	complex complex::operator=(const float n){
+		return complex(n, 0);
 	}
 
 	bool complex::operator==(const complex& src){
@@ -58,7 +54,7 @@ namespace ComplexLib
 			return false;
 	}
 	
-	bool complex::operator==(const double n){
+	bool complex::operator==(const float n){
 		float eps = 0.000001f;
 		if ((*this - n).abs() < eps)
 			return true;
@@ -66,93 +62,7 @@ namespace ComplexLib
 			return false;
 	}
 
-	double complex::abs(){
+	float complex::abs(){
 		return sqrt(x * x + y * y);
 	}
-
-	complex complex::pow(const int n){
-		complex c = *this;
-		for (int i = 0; i < n - 1; i++)
-			c = c * *this;
-		*this = c;
-		return *this;
-	}
-
-	//ComplexPolar
-
-	ComplexPolar ComplexPolar::operator+(const ComplexPolar& src){
-		return ComplexPolar(ToComplex(*this) + ToComplex(src));
-	}
-
-	ComplexPolar ComplexPolar::operator+(const double n){
-		return ComplexPolar(ToComplex(*this) + n);
-	}
-
-	ComplexPolar ComplexPolar::operator-(const ComplexPolar& src){
-		return ComplexPolar(ToComplex(*this) - ToComplex(src));
-	}
-
-	ComplexPolar ComplexPolar::operator-(const double n){
-		return ComplexPolar(ToComplex(*this) - n);
-	}
-
-	ComplexPolar ComplexPolar::operator*(const ComplexPolar& src){
-		return ComplexPolar(ToComplex(*this) * ToComplex(src));
-	}
-
-	ComplexPolar ComplexPolar::operator*(const double n){
-		return ComplexPolar(ToComplex(*this) * n);
-	}
-
-	ComplexPolar ComplexPolar::operator/(const ComplexPolar& src){
-		return ComplexPolar(ToComplex(*this) / ToComplex(src));
-	}
-
-	ComplexPolar ComplexPolar::operator/(const double n){
-		return ComplexPolar(ToComplex(*this) / n);
-	}
-
-	ComplexPolar ComplexPolar::operator=(const ComplexPolar& src){
-		r = ComplexPolar(src.r, src.fi).r;
-		fi = ComplexPolar(src.r, src.fi).fi;
-		return *this;
-	}
-
-	ComplexPolar ComplexPolar::operator=(const double n){
-		*this = ComplexPolar(complex(n, 0.0));
-		return *this;
-	}
-
-	bool ComplexPolar::operator==(const ComplexPolar& src){
-		float eps = 0.000001f;
-		if ((*this - src).abs() < eps)
-			return true;
-		else
-			return false;
-	}
-
-	bool ComplexPolar::operator==(const double n){
-		float eps = 0.000001f;
-		if ((*this - ComplexPolar(complex(n, 0.0))).abs() < eps)
-			return true;
-		else
-			return false;
-	}
-
-	double ComplexPolar::abs(){
-		return r;
-	}
-
-	complex ComplexPolar::pow(const int n){
-		double r1 = r;
-		for (int i = 0; i < n - 1; i++)
-			r *= r1;
-		fi *= n;
-		return ToComplex(*this);
-	}
-
-	complex ComplexPolar::ToComplex(const ComplexPolar& src){
-		return complex(src.r * cos(src.fi), src.r * sin(src.fi));
-	}
-	
 }
